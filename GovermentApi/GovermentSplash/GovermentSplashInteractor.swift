1ba18daaf0a1f3eaea26c950a1d25ae96ea3d999
Adrian_Velazquez <adrian.velazquezo96@gmail.com>
2023-12-14 20:18:36 -0600

    [UPDATE] saving data into database finished

--- GovermentApi/GovermentSplash/GovermentSplashInteractor.swift
+++ GovermentApi/GovermentSplash/GovermentSplashInteractor.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 class GovermentSplashInteractor {
     var presenter: GovermentSplashPresenterProtocol?
@@ -20,13 +21,13 @@ extension GovermentSplashInteractor: GovermentSplashInteractorProtocol {
             
             let task = URLSession.shared.dataTask(with: urlRequest) { responseData, responseCode, error in
                 if let httpResponse = responseCode as? HTTPURLResponse {
-                        let statusCode = httpResponse.statusCode
-                        if statusCode != 200 {
-                            DispatchQueue.main.async {
-                                self.presenter?.responseErrorInfo(error: "Looks like there is a problem, error code \(statusCode)")
-                            }
+                    let statusCode = httpResponse.statusCode
+                    if statusCode != 200 {
+                        DispatchQueue.main.async {
+                            self.presenter?.responseErrorInfo(error: "Looks like there is a problem, error code \(statusCode)")
                         }
                     }
+                }
                 if let error = error {
                     DispatchQueue.main.async {
                         self.presenter?.responseErrorInfo(error: error.localizedDescription)
@@ -37,7 +38,7 @@ extension GovermentSplashInteractor: GovermentSplashInteractorProtocol {
                     do {
                         let jsonResponse = try JSONDecoder().decode(Response.self, from: response)
                         DispatchQueue.main.async {
-                            self.presenter?.responseInfo(responseData: jsonResponse)
+                            self.saveData(responseData: jsonResponse)
                         }
                     } catch let decodingError {
                         DispatchQueue.main.async {
@@ -47,6 +48,35 @@ extension GovermentSplashInteractor: GovermentSplashInteractorProtocol {
                 }
             }
             task.resume()
+        }
+    }
+    
+    private func saveData(responseData: Response) {
+        let db = Firestore.firestore()
+        
+        for result in responseData.results {
+            let resultData: [String: Any] = [
+                "id": result.id,
+                "dateInsert": result.dateInsert,
+                "slug": result.slug,
+                "columns": result.columns,
+                "fact": result.fact,
+                "organization": result.organization,
+                "resource": result.resource,
+                "url": result.url,
+                "operations": result.operations,
+                "dataset": result.dataset,
+                "createdAt": result.createdAt
+            ]
+            
+            let resultRef = db.collection("results").document(result.id)
+            resultRef.setData(resultData) { error in
+                if let error = error {
+                    self.presenter?.responseErrorInfo(error: error.localizedDescription)
+                } else {
+                    self.presenter?.responseInfo(responseData: responseData)
+                }
+            }
         }
     }
 }
